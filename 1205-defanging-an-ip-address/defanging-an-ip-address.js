8cca83dfd0d6db2e89f5de75d3adbc199f3cd6ee
Muhammedanas7474 <muhammedanas247474@gmail.com>
2025-09-11 09:43:38 +0530

    Time: 41 ms (56.03%) | Memory: 53.3 MB (65.90%) - LeetSync

--- 1205-defanging-an-ip-address/defanging-an-ip-address.js
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.js
@@ -3,14 +3,5 @@
  * @return {string}
  */
 var defangIPaddr = function(address) {
-    res=""
-    let add=address.split("")
-    for(x of add){
-        if(x==="."){
-            res +="[.]"
-        }else{
-            res +=x
-        }
-    }
-    return res
+    return address.replaceAll(".","[.]")
 };
