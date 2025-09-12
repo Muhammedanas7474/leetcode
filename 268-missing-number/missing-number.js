7f86bcf7430484655021199f0e62c1cf0f6f7b32
Muhammedanas7474 <muhammedanas247474@gmail.com>
2025-09-12 10:03:23 +0530

    Time: 221 ms (5.30%) | Memory: 55.9 MB (55.83%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -4,12 +4,11 @@
  */
 var missingNumber = function(nums) {
     let n=nums.length
-    let res=0
     for(i=0;i<=n;i++){
         if(!nums.includes(i)){
-            res=i
+            return i
         }
     }
-    return res
-    
+  
+  
 };
