669de95a52f11ab012206e9e0a6004a589df054a
Muhammedanas7474 <muhammedanas247474@gmail.com>
2025-11-07 10:14:20 +0530

    Time: 2 ms (20.26%) | Memory: 17.8 MB (59.71%) - LeetSync

--- 258-add-digits/add-digits.py
+++ 258-add-digits/add-digits.py
@@ -1,12 +1,16 @@
 class Solution:
     def addDigits(self, num: int) -> int:
         
-       while num >= 10:               
-            current_sum = 0
-            for i in str(num):         
-                current_sum += int(i)  
-            num = current_sum          
-       return num
+    #    while num >= 10:               
+    #         current_sum = 0
+    #         for i in str(num):         
+    #             current_sum += int(i)  
+    #         num = current_sum          
+    #    return num
+
+        while num >=10:
+            num= sum(int(i)for i in str(num))
+        return num
 
 
 
