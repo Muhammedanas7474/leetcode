f75a31efc8b86fc977193b4b6153e2f366775c36
Muhammedanas7474 <muhammedanas247474@gmail.com>
2026-01-01 12:13:20 +0530

    Time: 2 ms (42.66%) | Memory: 17.3 MB (89.03%) - LeetSync

--- 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
+++ 2844-sum-of-squares-of-special-elements/sum-of-squares-of-special-elements.py
@@ -1,12 +1,20 @@
 class Solution:
     def sumOfSquares(self, nums: List[int]) -> int:
+        # sum=0
+        # n=len(nums)
+        # for i,v in enumerate(nums):
+        #     idx=i+1
+        #     if n % idx == 0:
+        #         sum+=v*v
+        # return sum
+
         sum=0
         n=len(nums)
-        for i,v in enumerate(nums):
+
+        for i in range(n):
             idx=i+1
 
             if n % idx == 0:
-                sum+=v*v
-                
+                sum += nums[i] * nums[i]
         return sum
         
