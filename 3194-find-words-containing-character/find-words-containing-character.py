132878f3aee886bcfeb677d6b954ac5bc704f31e
Muhammedanas7474 <muhammedanas247474@gmail.com>
2026-01-08 22:47:14 +0530

    Time: 7 ms (5.90%) | Memory: 19.4 MB (16.27%) - LeetSync

---
+++ 3194-find-words-containing-character/find-words-containing-character.py
@@ -0,0 +1,10 @@
+class Solution:
+    def findWordsContaining(self, words: List[str], x: str) -> List[int]:
+        res=[]
+        for i,j in enumerate(words):
+            print(i)
+            print(j)
+            if x in j:
+                res.append(i)
+        return res
+        
