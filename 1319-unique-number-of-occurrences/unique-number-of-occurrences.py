3e3d3ba1bd312778984d41d1efc05e1b5180ae37
Muhammed Anas k <muhammedanas247474@gmail.com>
2026-04-13 15:56:52 +0530

    Time: 4 ms (7.77%) | Memory: 19.4 MB (19.72%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.py
@@ -1,18 +1,22 @@
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        res={}
+        # res={}
         
-        for i in arr:
-            if i in res:
-                res[i]=res[i]+1
-            else:
-                res[i]=1
+        # for i in arr:
+        #     if i in res:
+        #         res[i]=res[i]+1
+        #     else:
+        #         res[i]=1
 
-        seen_counts=[]
-        for i in res.values():
-            if i in seen_counts:
-                return False
-            seen_counts.append(i)
-        return True
+        # seen_counts=[]
+        # for i in res.values():
+        #     if i in seen_counts:
+        #         return False
+        #     seen_counts.append(i)
+        # return True
+
+
+        counts=Counter(arr).values()
+        return len(counts) == len(set(counts))
             
             
